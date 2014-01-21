104cc927ee7b112c9da96aa9624f5220079fa30f
Yad Faeq <yad.faiq@gmail.com>
2014-01-21 14:37:46 -0800

    publish to removed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,12 +7,6 @@ class ApplicationController < ActionController::Base
        current_user.activities.create! action: action, trackable: trackable
      end
 
-  def set_access_control_headers
-  	headers['Access-Control-Allow-Origin'] = 'http://tranquil-castle-6757.herokuapp.com/faye'
-    headers['Access-Control-Request-Method'] = %w{GET POST OPTIONS}.join(",")
-    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-  	headers['Access-Control-Max-Age'] = "1728000"
-  end
 
 end
 
