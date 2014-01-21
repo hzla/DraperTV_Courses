06862f49e1d59cb8ffd8cbea61ece85270fa129a
Kelsey Whelan <kelsey@kelseywhelan.com>
2014-01-21 13:38:39 -0800

    deleted after filter in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-     after_filter :set_access_control_headers
+     #after_filter :set_access_control_headers
 
    protect_from_forgery
 
@@ -8,12 +8,6 @@ private
      current_user.activities.create! action: action, trackable: trackable
    end
 
-def set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = 'http://tranquil-castle-6757.herokuapp.com/faye/faye'
-    headers['Access-Control-Request-Method'] = %w{GET POST OPTIONS}.join(",")
-    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-    headers['Access-Control-Max-Age'] = "1728000"
-end
 
 end
 
