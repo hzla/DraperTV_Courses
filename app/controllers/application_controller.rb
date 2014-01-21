1037bf2efb7e0af7867c0baa9897b4391e3ab1df
Yad Faeq <yad.faiq@gmail.com>
2014-01-20 17:17:54 -0800

    this is for staging Commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,11 @@ private
     current_user.activities.create! action: action, trackable: trackable
   end
 
-	after_filter headers['Access-Control-Allow-Origin'] = "http://tranquil-castle-6757.herokuapp.com/faye"
+after_filter :set_access_control_headers
+
+def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = "http://tranquil-castle-6757.herokuapp.com/faye"
+    headers['Access-Control-Request-Method'] = %w{GET POST OPTIONS}.join(",")
+end
+
 end
