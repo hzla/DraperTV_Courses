60ae3e42329ea268a8ec80da44ff6e82d3075875
Kelsey Whelan <kelsey@kelseywhelan.com>
2014-01-21 10:58:36 -0800

    added access headers to application controller and changed faye to draperuniversity domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,19 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+     after_filter :set_access_control_headers
+
+   protect_from_forgery
 
 private
-  def track_activity(trackable, action = params[:action])
-    current_user.activities.create! action: action, trackable: trackable
-  end
+   def track_activity(trackable, action = params[:action])
+     current_user.activities.create! action: action, trackable: trackable
+   end
 
+def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = 'http://faye.draperuniversity.com/faye'
+    headers['Access-Control-Request-Method'] = %w{GET POST OPTIONS}.join(",")
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Max-Age'] = "1728000"
+end
 
 end
+
