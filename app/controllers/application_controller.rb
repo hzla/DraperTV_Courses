417b2dddc30d8f1ed978f5435a56b4a7f020a0cb
Yad Faeq <yad.faiq@gmail.com>
2014-01-20 17:34:08 -0800

    this is for staging Commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,10 @@ private
 after_filter :set_access_control_headers
 
 def set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = "http://staging-online.draperuniversity.com"
+  	headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Request-Method'] = %w{GET POST OPTIONS}.join(",")
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+  	headers['Access-Control-Max-Age'] = "1728000"
 end
 
 end
