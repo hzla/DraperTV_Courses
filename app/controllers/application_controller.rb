80e9969a00841d23df9f25b429dcc1dab90eabf0
Yad Faeq <yad.faiq@gmail.com>
2014-01-20 17:23:22 -0800

    this is for staging Commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ private
 after_filter :set_access_control_headers
 
 def set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = "http://tranquil-castle-6757.herokuapp.com/faye"
+    headers['Access-Control-Allow-Origin'] = "http://staging-online.draperuniversity.com"
     headers['Access-Control-Request-Method'] = %w{GET POST OPTIONS}.join(",")
 end
 
