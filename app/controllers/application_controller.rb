76d90e5f92cc53897377e8a01fda55a7e85b8e90
Yad Faeq <yad.faiq@gmail.com>
2014-01-20 22:09:19 -0800

    this is for staging Commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+	after_filter :set_access_control_headers
+
   protect_from_forgery
 
 private
@@ -6,10 +8,8 @@ private
     current_user.activities.create! action: action, trackable: trackable
   end
 
-after_filter :set_access_control_headers
-
 def set_access_control_headers
-  	headers['Access-Control-Allow-Origin'] = '*'
+  	headers['Access-Control-Allow-Origin'] = 'http://tranquil-castle-6757.herokuapp.com/faye'
     headers['Access-Control-Request-Method'] = %w{GET POST OPTIONS}.join(",")
     headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
   	headers['Access-Control-Max-Age'] = "1728000"
