67e5f21d5a157fb598e69c4af81dec36087206ab
Yad Faeq <yad.faiq@gmail.com>
2014-01-21 20:19:04 -0800

    removed changed After Filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,12 @@ class ApplicationController < ActionController::Base
        current_user.activities.create! action: action, trackable: trackable
      end
 
+  def set_access_control_headers
   	headers['Access-Control-Allow-Origin'] = 'http://aqueous-caverns-4317.herokuapp.com/faye'
     headers['Access-Control-Request-Method'] = %w{GET POST OPTIONS}.join(",")
     headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
   	headers['Access-Control-Max-Age'] = "1728000"
+  end
 
 end
 
