8f1187c757993a879d697962ba876d4b89ecda95
Kelsey Whelan <kelsey@kelseywhelan.com>
2014-01-27 13:26:25 -0800

    took out commenting for assignments, need to change to user_assignments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,24 +4,32 @@ class ApplicationController < ActionController::Base
   before_filter :cors_preflight_check
   after_filter :cors_set_access_control_headers
   # For all responses in this controller, return the CORS access control headers.
-  def cors_set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = 'http://aqueous-caverns-4317.herokuapp.com/faye'
-    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-    headers['Access-Control-Request-Method'] = 'http://aqueous-caverns-4317.herokuapp.com/faye'
-    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  # def cors_set_access_control_headers
+  #   headers['Access-Control-Allow-Origin'] = 'http://aqueous-caverns-4317.herokuapp.com/faye'
+  #   headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+  #   headers['Access-Control-Request-Method'] = 'http://aqueous-caverns-4317.herokuapp.com/faye'
+  #   headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  # end
+  # # If this is a preflight OPTIONS request, then short-circuit the
+  # # request, return only the necessary headers and return an empty
+  # # text/plain.
+  # def cors_preflight_check
+  #   if request.method == :options
+  #     headers['Access-Control-Allow-Origin'] = 'http://aqueous-caverns-4317.herokuapp.com/faye'
+  #     headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+  #     headers['Access-Control-Request-Method'] = 'http://aqueous-caverns-4317.herokuapp.com/faye'
+  #     headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  #     render :text => '', :content_type => 'text/plain'
+  #   end
+  # end
+
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = 'http://ancient-fjord-6459.herokuapp.com/faye'
+    headers['Access-Control-Request-Method'] = %w{GET POST OPTIONS}.join(",")
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Max-Age'] = "1728000"
   end
-  # If this is a preflight OPTIONS request, then short-circuit the
-  # request, return only the necessary headers and return an empty
-  # text/plain.
-  def cors_preflight_check
-    if request.method == :options
-      headers['Access-Control-Allow-Origin'] = 'http://aqueous-caverns-4317.herokuapp.com/faye'
-      headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-      headers['Access-Control-Request-Method'] = 'http://aqueous-caverns-4317.herokuapp.com/faye'
-      headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-      render :text => '', :content_type => 'text/plain'
-    end
-  end
+
 
   private
      def track_activity(trackable, action = params[:action])
@@ -29,15 +37,8 @@ class ApplicationController < ActionController::Base
      end
 
 
-  #### Yad, do we need this? ####
   
-  # def set_access_control_headers
-  # 	headers['Access-Control-Allow-Origin'] = 'http://ancient-fjord-6459.herokuapp.com/faye'
-  #   headers['Access-Control-Request-Method'] = %w{GET POST OPTIONS}.join(",")
-  #   headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-  # 	headers['Access-Control-Max-Age'] = "1728000"
-  # end
-
+  
 
 end
 
