66bcd49740f671875dbfe638129685a078eef326
Yad Faeq <yad.faiq@gmail.com>
2014-04-07 17:30:19 -0700

    removed Timezones Setting, it causes dateTime Error

--- config/routes.rb
+++ config/routes.rb
@@ -94,8 +94,10 @@ OnlineSchool::Application.routes.draw do
 
   post '/assignments/:id/quiz_save_attempt', to: 'assignments#quiz_save_attempt', as: :assignments_survey_attempts
 
-  match '(errors)/:status', to: 'errors#show', constraints: {status: /\d{3}/ }, via: :all
+  # match '(errors)/:status', to: 'errors#show', constraints: {status: /\d{3}/ }, via: :all
+  match '(errors)/:status', to: 'errors#show', constraints: {status: /\d{3}/}, via: :get
   match "/delayed_jobs" => DelayedJobWeb, :anchor => false, via: [:get, :post]
+
 
 
   # get "profile/show"
