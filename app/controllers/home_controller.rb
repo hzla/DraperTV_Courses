9a320a2b850f30994fbf2a0b4517044177c70758
Kelsey Whelan <kelsey@kelseywhelan.com>
2014-04-28 11:49:20 -0700

    home controller redirects to logout_path if user signed in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     if user_signed_in?
       #redirect_to posts_path, :use_rails_ajax => false
-      redirect_to login_path, :use_rails_ajax => false
+      redirect_to logout_path, :use_rails_ajax => false
     else
       redirect_to login_path, :use_rails_ajax => false
     end
