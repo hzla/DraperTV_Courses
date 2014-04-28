066772e6bcec33942cb8aadd9169166b2478b099
Kelsey Whelan <kelsey@kelseywhelan.com>
2014-04-28 11:28:42 -0700

    add timeoutable method to devise user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
   friendly_id :full_name, use: [:slugged, :finders]
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :timeoutable
 
 
   has_attached_file :avatar,
