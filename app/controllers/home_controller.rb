c215f4a83fe8f44b373c6109d570070c178e817a
Kelsey Whelan <kelsey@kelseywhelan.com>
2014-05-22 14:13:35 -0700

    updated path in home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     if user_signed_in?
-      redirect_to activity_feed_path, :use_rails_ajax => false
+      redirect_to activity_feeds_path, :use_rails_ajax => false
     else
       redirect_to login_path, :use_rails_ajax => false
     end
