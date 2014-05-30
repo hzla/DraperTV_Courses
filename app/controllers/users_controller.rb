9c06b757ef37fc8c046318cbd4dcfbcc1e76c79f
Yad Faeq <yad.faiq@gmail.com>
2014-05-30 11:36:04 -0700

    Added Work Report limit to 5 for Test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   end
 
   def workreport
-    @users = User.all
+    @users = User.all.limit(5)
     @courses = Course.all
     @assignments = Assignment.all
     @user_assignments = UserAssignment.all
