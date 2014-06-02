dbea7a9a33b9aa7a80095c9c0d5722d2d31077fa
Yad Faeq <yad.faiq@gmail.com>
2014-06-02 11:57:56 -0700

    Fixed Delete comment for the Live Update rendering

--- app/controllers/user_comments_controller.rb
+++ app/controllers/user_comments_controller.rb
@@ -19,25 +19,25 @@ class UserCommentsController < ApplicationController
       end
   end
 
-def vote_for_comment
-  begin
-    #@comment = UserComment.find(params[:id])
-    current_user.vote_for(@comment = UserComment.find(params[:id]))
-    redirect_to :back
-  rescue
-    render :nothing => true, :status => 404
+  def vote_for_comment
+    begin
+      #@comment = UserComment.find(params[:id])
+      current_user.vote_for(@comment = UserComment.find(params[:id]))
+      redirect_to :back
+    rescue
+      render :nothing => true, :status => 404
+    end
   end
-end
 
-def unvote_for_comment
-  begin
-    @comment = UserComment.find(params[:id])
-    current_user.unvote_for(@comment)
-    redirect_to :back
-  rescue
-    render :nothing => true, :status => 404
+  def unvote_for_comment
+    begin
+      @comment = UserComment.find(params[:id])
+      current_user.unvote_for(@comment)
+      redirect_to :back
+    rescue
+      render :nothing => true, :status => 404
+    end
   end
-end
 
   def new
     @comment = @commentable.user_comments.new()
@@ -103,138 +103,183 @@ end
 
   end
 
-def notifSend(comment)
-  begin
-  @comments = UserComment.all
-      if @comment.commentable_type == "Post"
-          listUsers = []
-         @posts = Post.all
+  def notifSend(comment)
+    begin
+    @comments = UserComment.all
+        if @comment.commentable_type == "Post"
+            listUsers = []
+           @posts = Post.all
 
+            @comments.each do |com|
+              if com.commentable_id ==  @comment.commentable_id
+                listUsers << com.user_id
+              end
+            end
+
+            @posts.each do |po|
+              if po.id ==  @comment.commentable_id
+                listUsers << po.user_id
+              end
+            end
+
+            @posts.each do |po|
+              if po.user_id ==  @comment.user_id
+                listUsers << po.user_id
+              end
+            end
+
+            @topic = Post.find_by_id(@comment.commentable_id)
+            @commenter = User.find_by_id(@comment.user_id)
+            listUsers << @topic.user_id
+
+            listUsers = listUsers.uniq
+            listUsers.delete(current_user.id)
+            listUsers.each do |usr|
+              @user = User.find(usr)
+              @user.increment!(:ncounter)
+              PrivatePub.publish_to("/layouts/#{usr}",
+                "$('#notifications').removeClass('empty');
+                $('#notification').addClass('notifications');
+                $('.red-circle').show();
+                $('.red-circle p').text(#{@user.ncounter});")
+            end
+        elsif @comment.commentable_type == "Assignment"
+
+          @assignment = UserAssignment.all
+
+          listUsers = []
           @comments.each do |com|
             if com.commentable_id ==  @comment.commentable_id
               listUsers << com.user_id
             end
           end
 
-          @posts.each do |po|
+          @assignment = UserAssignment.all
+          @assignment.each do |po|
             if po.id ==  @comment.commentable_id
               listUsers << po.user_id
             end
           end
 
-          @posts.each do |po|
+          @assignment = UserAssignment.all
+          @assignment.each do |po|
             if po.user_id ==  @comment.user_id
               listUsers << po.user_id
             end
           end
 
-          @topic = Post.find_by_id(@comment.commentable_id)
+          @topic = UserAssignment.find_by_assignment_id(@comment.commentable_id)
+
           @commenter = User.find_by_id(@comment.user_id)
           listUsers << @topic.user_id
-
           listUsers = listUsers.uniq
           listUsers.delete(current_user.id)
+
           listUsers.each do |usr|
-            @user = User.find(usr)
-            @user.increment!(:ncounter)
-            PrivatePub.publish_to("/layouts/#{usr}",
-              "$('#notifications').removeClass('empty');
-              $('#notification').addClass('notifications');
-              $('.red-circle').show();
-              $('.red-circle p').text(#{@user.ncounter});")
+            if User.find_by_id(usr).present?
+              @user = User.find(usr)
+              @user.increment!(:ncounter)
+              PrivatePub.publish_to("/layouts/#{usr}",
+                "$('#notifications').removeClass('empty');
+                $('#notification').addClass('notifications');
+                $('.red-circle').show();
+                $('.red-circle p').text(#{@user.ncounter});")
+            end
           end
-      elsif @comment.commentable_type == "Assignment"
+        elsif @comment.commentable_type == "Event"
+          @posts = Event.all
 
-        @assignment = UserAssignment.all
-
-        listUsers = []
-        @comments.each do |com|
-          if com.commentable_id ==  @comment.commentable_id
-            listUsers << com.user_id
+          listUsers = []
+          @comments = UserComment.all
+          @comments.each do |com|
+            if com.commentable_id ==  @comment.commentable_id
+              listUsers << com.user_id
+            end
           end
+
+          @posts = Event.all
+          @posts.each do |po|
+            if po.id ==  @comment.commentable_id
+              listUsers << po.user_id
+            end
+          end
+          @posts = Event.all
+          @posts.each do |po|
+            if po.user_id ==  @comment.user_id
+              listUsers << po.user_id
+            end
+          end
+          @topic = Event.find_by_id(@comment.commentable_id)
+
+          @commenter = User.find_by_id(@comment.user_id)
+          listUsers << @topic.user_id
+          listUsers = listUsers.uniq
+          listUsers.delete(current_user.id)
+
+          listUsers.each do |usr|
+            if User.find_by_id(usr).present?
+              @user = User.find(usr)
+              @user.increment!(:ncounter)
+              PrivatePub.publish_to("/layouts/#{usr}",
+                "$('#notifications').removeClass('empty');
+                 $('#notification').addClass('notifications');
+                 $('.red-circle').show();
+                 $('.red-circle p').text(#{@user.ncounter});")
+            end
+
+          end
+        else
+        end # end of if @comment.commentable_type block
+      rescue => exception
+      ExceptionNotifier.notify_exception(exception)
+      end
+  end
+  def destroy
+    @comments = @commentable.user_comments.order('created_at desc')
+    @comment = UserComment.find(params[:id])
+    @user = User.find(@comment.user_id)
+    @words =  @user.char_points.to_i - @comment.content.split.size.to_i
+    @user.update_column(:char_points, 1 * @words)
+    @user.update_column(:pcounter, @user.pcounter.to_i - @comment.content.split.size.to_i)
+
+    @activities = Activity.all
+    @activities.find_by_trackable_id(@comment.id).destroy
+
+      @comments.each do |com|
+        if com.commentable_id == @comment.id
+            @activities.find_by_trackable_id(com.id).destroy
         end
-
-        @assignment = UserAssignment.all
-        @assignment.each do |po|
-          if po.id ==  @comment.commentable_id
-            listUsers << po.user_id
+      end
+    if current_user.id == @comment.user_id
+      @comment.destroy
+        @comments = @commentable.user_comments.order('created_at desc')
+        #refresh_dom_with_partial('div#comments_container', 'comments')
+          if @comment.commentable_type == "Assignment"
+            respond_to do |format|
+              format.html {  }
+              format.js { render :commentAssignment }
+            end
+          elsif @comment.commentable_type == "UserAssignment"
+            @comments = @commentable.user_comments.order('created_at asc')
+            @user_assignment = UserAssignment.where(:id => @comment.commentable_id).first
+            respond_to do |format|
+              format.html {  }
+              format.js { render :commentUA }
+            end
+          elsif @comment.commentable_type == "Event"
+            @comments = @commentable.user_comments.order('created_at asc')
+            respond_to do |format|
+              format.html {  }
+              format.js { render :commentEvent }
+            end
+          else
+            respond_to do |format|
+              format.html { }
+              format.js { render :commentVote }
+            end
           end
-        end
-
-        @assignment = UserAssignment.all
-        @assignment.each do |po|
-          if po.user_id ==  @comment.user_id
-            listUsers << po.user_id
-          end
-        end
-
-        @topic = UserAssignment.find_by_assignment_id(@comment.commentable_id)
-
-        @commenter = User.find_by_id(@comment.user_id)
-        listUsers << @topic.user_id
-        listUsers = listUsers.uniq
-        listUsers.delete(current_user.id)
-
-        listUsers.each do |usr|
-          if User.find_by_id(usr).present?
-            @user = User.find(usr)
-            @user.increment!(:ncounter)
-            PrivatePub.publish_to("/layouts/#{usr}",
-              "$('#notifications').removeClass('empty');
-              $('#notification').addClass('notifications');
-              $('.red-circle').show();
-              $('.red-circle p').text(#{@user.ncounter});")
-          end
-        end
-      elsif @comment.commentable_type == "Event"
-        @posts = Event.all
-
-        listUsers = []
-        @comments = UserComment.all
-        @comments.each do |com|
-          if com.commentable_id ==  @comment.commentable_id
-            listUsers << com.user_id
-          end
-        end
-
-        @posts = Event.all
-        @posts.each do |po|
-          if po.id ==  @comment.commentable_id
-            listUsers << po.user_id
-          end
-        end
-        @posts = Event.all
-        @posts.each do |po|
-          if po.user_id ==  @comment.user_id
-            listUsers << po.user_id
-          end
-        end
-        @topic = Event.find_by_id(@comment.commentable_id)
-
-        @commenter = User.find_by_id(@comment.user_id)
-        listUsers << @topic.user_id
-        listUsers = listUsers.uniq
-        listUsers.delete(current_user.id)
-
-        listUsers.each do |usr|
-          if User.find_by_id(usr).present?
-            @user = User.find(usr)
-            @user.increment!(:ncounter)
-            PrivatePub.publish_to("/layouts/#{usr}",
-              "$('#notifications').removeClass('empty');
-               $('#notification').addClass('notifications');
-               $('.red-circle').show();
-               $('.red-circle p').text(#{@user.ncounter});")
-          end
-
-        end
-      else
-      end # end of if @comment.commentable_type block
-    rescue => exception
-    ExceptionNotifier.notify_exception(exception)
     end
-end
-
+  end
 
 private
 
