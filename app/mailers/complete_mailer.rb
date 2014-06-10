9f7ea302e199ee46b25349c542f5cfdb84372d1a
Kelsey Whelan <kelsey@kelseywhelan.com>
2014-06-09 18:03:54 -0700

    public assets for vimeo congrats videos

--- app/mailers/complete_mailer.rb
+++ app/mailers/complete_mailer.rb
@@ -7,14 +7,13 @@ class CompleteMailer < ActionMailer::Base
     @user = user
     @course = Course.find(@badge.course_id)
     mail(
-      :to => "kelsey@draperuniversity.com",
+      :to => "kelsey@draperuniversity.com, kelseymwhelan@gmail.com",
       :subject => "Congratulations!",
       :from => 'Draper University',
       :date => Time.now,
-      :content_type => "text/html",
+
       :template_path => "complete_mailer",
       :template_name => "course_complete"
     )
-    #mail( :to => "duhonlineapps@gmail.com, applications@draperuniversity.com", :subject => "New Boarding School Application: #{@app.first} #{@app.last}" )
   end
 end
