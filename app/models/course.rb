4db5883565d450eac70c9f1fe0fc1cc2b9b433e1
Kelsey Whelan <kelsey@kelseywhelan.com>
2014-06-20 12:00:26 -0700

    minor changes

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,7 +3,7 @@ class Course < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, use: [:slugged, :finders]
   attr_accessible :description, :title, :start_date, :end_date, :intro_vimeo, :length, :badge_vimeo, :tile_image
-  attr_accessible :course_icon, :intro_screenshot
+  attr_accessible :course_icon, :intro_screenshot, :syllabus
   has_attached_file :course_icon,
     :styles => { :large => "200x200#", :badge => "80x80#" },
     :default_url => "/images/icon_missing.png",
@@ -19,6 +19,10 @@ class Course < ActiveRecord::Base
     :storage => :s3,
     :s3_credentials => "#{Rails.root}/config/s3.yml",
     :styles => { :medium => "260x320#", :thumb => "80x80#" }
+  # has_attached_file :syllabus,
+  #   :bucket => 'duhonline',
+  #   :storage => :s3,
+  #   :s3_credentials => "#{Rails.root}/config/s3.yml"
   has_many :assignments
 
   def assignment_count(current_user)
