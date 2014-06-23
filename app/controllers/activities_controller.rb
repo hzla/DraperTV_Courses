a622fcaa4005f065fd567fdaa46b45691781e58f
Yad Faeq <yad.faiq@gmail.com>
2014-06-23 13:56:24 -0700

    fixed Posts list for notification List to go through

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -3,7 +3,7 @@ class ActivitiesController < ApplicationController
   load_and_authorize_resource
   def index
   	@activities =  Activity.all
-    @activities = Activity.page(params[:page]).per(10).order("created_at desc").includes(:trackable)
+    @activities = Activity.where.not(user_id: current_user.id).page(params[:page]).per(10).order("created_at desc").includes(:trackable)
     @posts = Post.all
     @comments = UserComment.all
 
@@ -14,6 +14,13 @@ class ActivitiesController < ApplicationController
          @commentableIDs << com.commentable_id
        end
      end
+
+     @posts.each do |post|
+       if post.user_id ==  current_user.id
+         @commentableIDs << post.id
+       end
+     end
+
     #End of the list that contains Object Ids that this User commented on
 
 
