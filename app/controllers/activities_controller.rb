085564b183a014702e9264370ce09c08c481b7b4
Yad Faeq <yad.faiq@gmail.com>
2014-06-25 16:41:24 -0700

    fixed the list for notifications

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,35 +2,50 @@ class ActivitiesController < ApplicationController
   before_filter :authenticate_user!
   load_and_authorize_resource
   def index
+
   	@activities =  Activity.all
-    @activities = Activity.where.not(user_id: current_user.id).page(params[:page]).per(10).order("created_at desc").includes(:trackable)
     @posts = Post.all
     @comments = UserComment.all
 
+     @commentableIDs = []
+     @comments.each do |com|
+       if com.user_id ==  current_user.id
+         @commentableIDs << com.commentable_id
+       end
+     end
+
+     @posts.each do |post|
+       if post.user_id ==  current_user.id
+         @commentableIDs << post.id
+       end
+     end
+     @commentableIDs = @commentableIDs.uniq
+
+  @trackableIDs = []
+    @activities.each do |activity|
+      @trackable = activity.trackable_type
+      arr = eval(@trackable)
+      if @commentableIDs.include? UserComment.where(:id => activity.trackable_id).first.commentable_id
+         @trackableIDs << activity.trackable_id
+       end
+    end
+
+    @activities = Activity.order("created_at desc").where("trackable_id IN (?)", @trackableIDs).where.not(:user_id => current_user.id).page(params[:page]).per(10).includes(:trackable)
+
+# .where("user_id NOT IN (?)", User.where(:role => "admin").pluck(:id))
     #Creating a list of objects that the current user have COMMENTED on
 
-  @commentableIDs = []
-     @comments.each do |com|
-         @commentableIDs << com.commentable_id
+    # @commentableIDs = []
+    # @comments.each do |com|
+    #    @commentableIDs << com.commentable_id
+    # end
 
-     end
-     @posts.each do |post|
-         @commentableIDs << post.id
-     end
+    # @posts.each do |post|
+    #    @commentableIDs << post.id
+    # end
 
 
-     # @commentableIDs = []
-     # @comments.each do |com|
-     #   if com.user_id ==  current_user.id
-     #     @commentableIDs << com.commentable_id
-     #   end
-     # end
-
-     # @posts.each do |post|
-     #   if post.user_id ==  current_user.id
-     #     @commentableIDs << post.id
-     #   end
-     # end
+    #activites where the posts relates to me
 
     #End of the list that contains Object Ids that this User commented on
 
