311f029dbba76ea3ff1d6f6ca727040a213563d6
Andy Lee <alee@andy.local>
2015-08-10 13:30:42 -0700

    update stripe keys to live

--- db/seeds/renaming_and_changes.rb
+++ db/seeds/renaming_and_changes.rb
@@ -6,7 +6,7 @@
 # Lesson.find_or_create_by lesson_type: "watch", video: true, description: "Have Difficult Conversations- Josh Buckley", video_author: author, video_title: title, track_id: track_id, video_uid: "135826441" 
 
 
-# topic = Topic.where(name: "ATTITUDE").first
+topic = Topic.where(name: "ATTITUDE").first
 # Track.where(name: "RESOURCEFULNESS").destroy_all
 persistence = Track.find_or_create_by topic_id: topic.id, name: "PERSISTENCE", icon: "persistence.svg", summary: "“Building a company means having a focused vision, and working against all odds and obstacles to achieve it.”"
 resourcefulness = Track.find_or_create_by topic_id: topic.id, name: "RESOURCEFULNESS", icon: "resourcefulness.svg" , summary: "Starting a startup means figuring out creative ways to get things done no matter how difficult they may seem."
