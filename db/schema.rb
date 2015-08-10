a3b4facabe57281e4deeeb9165b8e61d23c4226c
Andy Lee <alee@andy.local>
2015-08-10 16:45:45 -0700

    fix fb login

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150810033722) do
+ActiveRecord::Schema.define(version: 20150810234446) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -548,7 +548,7 @@ ActiveRecord::Schema.define(version: 20150810033722) do
     t.boolean  "show_track_tutorial",    default: true
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["slug"], name: "index_users_on_slug", using: :btree
 
