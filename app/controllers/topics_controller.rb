42c9d8c0f305cffdfa4fe7ad85b10e4919659673
Andy Lee <alee@andy.local>
2015-08-09 21:24:49 -0700

    fix redirect loop

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -8,7 +8,7 @@ class TopicsController < ApplicationController
     @page = "courses"
     @tutorial = params["tutorial"] == "true"
     if current_user && current_user.show_topic_tutorial
-      @user.update_attributes show_topic_tutorial: false
+      current_user.update_attributes show_topic_tutorial: false
       redirect_to topics_path(tutorial: true)
     end
   end
