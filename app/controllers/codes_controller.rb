c0efb17ef1646d46c220a06410d69a557c3ca8ed
Andy Lee <alee@andy.local>
2015-08-11 17:54:53 -0700

    fix dropdowna nd code

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -5,7 +5,7 @@ class CodesController < ApplicationController
   	code = Code.find_by_body_and_used(body, false)
   	if code
   		code.update_attributes used: true
-  		current_user.update_attributes paid: true
+  		current_user.update_attribute(:paid, true)
   		redirect_to root_path and return
   	else
   		redirect_to new_charge_path(bad_code: true)
