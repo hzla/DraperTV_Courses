c1866c8bc8a700fdcd40aeb2a51648756bea1b7d
Andy Lee <alee@andy.local>
2015-09-16 17:03:45 -0700

    remove deprecated models from ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -34,8 +34,6 @@ class Ability
         else
             can :read, :all
             can :manage, User, :id => user.id
-            can :manage, Event, :user_id => user.id
-            can :manage, Post, :user_id => user.id
         end
     end
 end
