3e97588bb9e9154ae54345216db4b94e3ab94c82
Andy Lee <alee@andy.local>
2015-10-15 17:30:54 -0700

    fix new password page

---
+++ app/admin/codes.rb
@@ -0,0 +1,48 @@
+ActiveAdmin.register Code do
+  index do
+    column :id
+    column :full_name
+    column :role
+    column :team
+    column :last_sign_in_at
+    column :email
+    actions
+  end
+
+  form do |f|
+   f.inputs "User Details" do
+     f.inputs :first_name
+     f.inputs :last_name
+     f.inputs :email
+     f.inputs :tag_list
+     f.inputs :team
+     f.inputs :bio
+     f.inputs :skype
+     f.inputs :gmail
+     f.inputs :twitter
+     f.inputs :facebook
+     f.inputs :linkedin
+     f.inputs :instagram
+     f.inputs :angellist
+     f.inputs :dribbble
+     f.inputs :github
+     f.inputs :program
+     f.inputs :online
+     f.inputs :street_address
+     f.inputs :city
+     f.inputs :state
+     f.inputs :zip
+     f.inputs :country
+     f.inputs :eventReminder
+     f.inputs :password
+     f.inputs :role
+     f.inputs :avatar,
+      :styles => { :medium => "120x120#", :thumb => "40x40#" },
+      :bucket => 'duhonline',
+      :storage => :s3,
+      :s3_credentials => "#{Rails.root}/config/s3.yml"
+   end
+  f.actions
+ end
+
+end
